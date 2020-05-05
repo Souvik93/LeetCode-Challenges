9ab5e7b6b718ee3c9ac63c8d7cb9752a10152c42
Souvik <souvik.das@live.in>
2020-05-06 00:05:42 +0530

    Code Updated.
    Working Code Updated....

--- UniqueCharacter_Day5.java
+++ UniqueCharacter_Day5.java
@@ -10,3 +10,33 @@ s = "loveleetcode",
 return 2.
 Note: You may assume the string contain only lowercase letters.
 */
+class Solution {
+    public int firstUniqChar(String s) {
+        if(s.length()<=0) {
+            return -1;
+        }
+        if(s.length() == 1) {
+            return 0;
+        }
+        
+        int last = s.length();
+        
+        for(int i=0;i<last;i++) {
+            String str;
+            if(i==last-1) {
+                str = s.substring(0,i);
+            }
+            else {
+                str = s.substring(0,i) + s.substring(i+1);
+            }
+            
+            if(!str.contains(s.charAt(i)+"")) {
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+}
+
